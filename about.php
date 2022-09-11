e6c42ac1b626382aa392404c16d108047714edb0
Anand Mohan <anand1505mohan@gmail.com>
2022-09-11 07:09:27 -0700

    aboutus completed

--- about.php
+++ about.php
@@ -6,6 +6,12 @@
     <title>Hotel Manglam - About Us</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
     <?php require('include/links.php') ?>
+    <style>
+        .box{
+           border-top-color: var(--teal) !important;
+        }
+
+    </style>
   </head>
   <body>
     <?php require('include/header.php') ?>
@@ -19,77 +25,98 @@
       </div>
 
       <div class="container">
+        <div class="row justify-content-between align-items-center">
+            <div class="col-lg-6 col-md-5 mb-4 order-lg-1 order-md-1 order-2">
+                <h3 class="mb-3">Lorem ipsum dolor sit.</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dolores unde maiores soluta provident possimus ullam?</p>
+            </div>
+            <div class="col-lg-5 col-md-5 mb-4 order-lg-2 order-md-2 order-1">
+                 <img src="images/about/about.jpg" class="w-100">
+            </div>
+        </div>
+      </div>
+
+      <div class="container mt-5">
         <div class="row">
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                    <img src="images/facilities/IMG_43553.svg" width="40px" alt="">
-                    <h5 class="m-0 ms-3">WiFi</h5>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Sit quas magnam numquam suscipit deleniti fuga!
-                    </p>
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+                    <img src="images/about/hotel.svg" width="70px" >
+                    <h4 class="mt-3">100+ Rooms</h4>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                    <img src="images/facilities/IMG_49949.svg" width="40px" alt="">
-                    <h5 class="m-0 ms-3">A.C.</h5>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Sit quas magnam numquam suscipit deleniti fuga!
-                    </p>
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+                    <img src="images/about/customers.svg" width="70px" >
+                    <h4 class="mt-3">200+Customers</h4>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                    <img src="images/facilities/IMG_47816.svg" width="40px" alt="">
-                    <h5 class="m-0 ms-3">Massage</h5>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Sit quas magnam numquam suscipit deleniti fuga!
-                    </p>
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+                    <img src="images/about/rating.svg" width="70px" >
+                    <h4 class="mt-3">1000+ Reviews</h4>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                    <img src="images/facilities/IMG_41622.svg" width="40px" alt="">
-                    <h5 class="m-0 ms-3">Television</h5>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Sit quas magnam numquam suscipit deleniti fuga!
-                    </p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                    <img src="images/facilities/IMG_49949.svg" width="40px" alt="">
-                    <h5 class="m-0 ms-3">WiFi</h5>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Sit quas magnam numquam suscipit deleniti fuga!
-                    </p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                    <img src="images/facilities/IMG_96423.svg" width="40px" alt="">
-                    <h5 class="m-0 ms-3">Heater</h5>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Sit quas magnam numquam suscipit deleniti fuga!
-                    </p>
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+                    <img src="images/about/staff.svg" width="70px" >
+                    <h4 class="mt-3">50+ Staffs</h4>
                 </div>
             </div>
         </div>
       </div>
 
+      <h3 class="my-5 fw-bold h-font text-center">MANAGEMENT TEAM</h3>
+
+      <div class="container px-4">
+            <div class="swiper mySwiper">
+                <div class="swiper-wrapper mb-5">
+                    <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+                        <img src="images/about/IMG_17352.jpg" class="w-100">
+                        <h5>Raju Shrivastav</h5>
+                    </div>
+                    <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+                        <img src="images/about/IMG_17352.jpg" class="w-100">
+                        <h5>Raju Shrivastav</h5>
+                    </div>
+                    <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+                        <img src="images/about/IMG_17352.jpg" class="w-100">
+                        <h5>Raju Shrivastav</h5>
+                    </div>
+                    <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+                        <img src="images/about/IMG_17352.jpg" class="w-100">
+                        <h5>Raju Shrivastav</h5>
+                    </div>
+                </div>
+                <div class="swiper-pagination"></div>
+            </div>
+      </div>
 
     <?php include('include/footer.php') ?>
+
+    <!-- Swiper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+    <!-- Initialize Swiper -->
+    <script>
+      var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 40,
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 1,
+          },
+          640: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+        }
+      });
+    </script>
   </body>
 </html>
