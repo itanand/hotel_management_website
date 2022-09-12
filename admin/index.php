f25ba816ff5e3f5b654e657dfcea2d73431359d8
Anand Mohan <anand1505mohan@gmail.com>
2022-09-12 08:41:59 -0700

    admin-page and images design

--- admin/index.php
+++ admin/index.php
@@ -8,13 +8,33 @@
     <?php
       require('inc/links.php');
     ?>
+    <style>
+        div.login-form{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 400px;
+        }
+    </style>
 </head>
 <body class="bg-light">
 
+   <div class="login-form text-center rounded bg-white shadow overflow-hidden">
+      <form action="">
+         <h4 class="bg-dark text-white py-3">ADMIN LOGIN PANEL</h4>
+            <div class="p-4">
+                <div class="mb-3">
+                   <input name="admin_name" type="text" class="form-control shadow-none text-center" placeholder="Admin Name" >
+                </div>
+                <div class="mb-4">
+                    <input name="admin_password" type="password" class="form-control shadow-none text-center" placeholder="Admin Password" >
+                </div>
+                <button type="Submit" class="btn text-white custom-bg shadow-none" name="login">LOGIN</button>
+            </div>
+      </form>
+   </div>
 
-
-    <?php
-    require('inc/scripts.php');
-    ?>
+<?php require('inc/scripts.php'); ?>
 </body>
 </html>
