9f1e84c54d91afc59345780000ba64c7601524d6
Anand Mohan <anand1505mohan@gmail.com>
2022-09-13 04:28:14 -0700

    db_config and admin

---
+++ admin/inc/db_config.php
@@ -0,0 +1,53 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "manglam_hotel";
+
+//connection
+
+$con = mysqli_connect("$host", "$username", "$password","$database");
+
+// check Connection
+if(!$con)
+{
+   die("Cannot connect to Database".mysqli_connect_error());
+}
+
+function filteration($data)
+{
+    foreach($data as $key => $value){
+        $data[$key] = trim($value);
+        $data[$key] = stripcslashes($value);
+        $data[$key] = htmlspecialchars($value);
+        $data[$key] = strip_tags($value);     //html tag remove
+    }
+    return $data;
+}
+
+function select($sql, $values, $datatypes)
+{
+   $con = $GLOBALS['con'];
+   if($stmt = mysqli_prepare($con,$sql))
+   {
+       mysqli_stmt_bind_param($stmt, $datatypes,...$values); //php splat operator
+       if(mysqli_stmt_execute($stmt ))
+       {
+        $res = mysqli_stmt_get_result($stmt);
+        mysqli_stmt_close($stmt);
+        return $res;
+       }
+       else{
+          mysqli_stmt_close($stmt);
+          die("Query Can't be executed - Select");
+       }
+
+   }
+   else
+   {
+    die("Query Can't be prepared - Select");
+   }
+
+}
+?>
--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,7 @@
+<?php
+require('inc/db_config.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,19 +25,33 @@
 <body class="bg-light">
 
    <div class="login-form text-center rounded bg-white shadow overflow-hidden">
-      <form action="">
+      <form action="" method="POST">
          <h4 class="bg-dark text-white py-3">ADMIN LOGIN PANEL</h4>
             <div class="p-4">
                 <div class="mb-3">
-                   <input name="admin_name" type="text" class="form-control shadow-none text-center" placeholder="Admin Name" >
+                   <input name="admin_name"  required type="text" class="form-control shadow-none text-center" placeholder="Admin Name" >
                 </div>
                 <div class="mb-4">
-                    <input name="admin_password" type="password" class="form-control shadow-none text-center" placeholder="Admin Password" >
+                    <input name="admin_pass" required type="password" class="form-control shadow-none text-center" placeholder="Admin Password" >
                 </div>
-                <button type="Submit" class="btn text-white custom-bg shadow-none" name="login">LOGIN</button>
+                <button type="Submit" class="btn btn-danger text-white shadow-none" name="login">LOGIN</button>
             </div>
       </form>
    </div>
+
+   <?php
+
+     if(isset($_POST['login']))
+     {
+        $frm_data =  filteration($_POST);
+
+        $query = "SELECT * FROM `admin_cred` WHERE `admin_name`=? AND `admin_pass`=?";
+        $values = [$frm_data['admin_name'],$frm_data['admin_pass']];  //value of arrays
+
+        $res = select($query, $values, "ss");
+        print_r($res);
+     }
+   ?>
 
 <?php require('inc/scripts.php'); ?>
 </body>
